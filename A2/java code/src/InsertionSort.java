c4cd3added342e4d31d1d7916eb91b036bee6ba4
mykothai <mike.thai2303@gmail.com>
2020-03-21 01:31:10 -0700

    init commit

---
+++ A2/java code/src/InsertionSort.java
@@ -0,0 +1,35 @@
+public class InsertionSort {
+    public int[] sort(int[] array) {
+        int length = array.length;
+
+        for (int i = 1; i < length; i++) {
+            int j = i;
+            while (j > 0 && array[j] < array[j - 1]) {
+                swap(array, j - 1, j);
+                j--;
+            }
+        }
+        return array;
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        int[] array = {4, 8, 5, 7, 2, 0, 9, 6, 1, 3};
+        insertionSort.printArray(array);
+        insertionSort.sort(array);
+        insertionSort.printArray(array);
+    }
+}
