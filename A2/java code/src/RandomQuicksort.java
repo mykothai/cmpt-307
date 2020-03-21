f73705ab6319f477c5528de07d5713d637df2cb8
mykothai <mike.thai2303@gmail.com>
2020-03-21 03:13:05 -0700

    Remove unused method

--- A2/java code/src/RandomQuicksort.java
+++ A2/java code/src/RandomQuicksort.java
@@ -52,13 +52,6 @@ public class RandomQuicksort {
         array[j] = temp;
     }
 
-    public void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
         RandomQuicksort randomQuicksort = new RandomQuicksort();
         // execution time measurements
