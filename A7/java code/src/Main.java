3a1a0d5ce4d2fa349c5af1f5db02ce40e2cdb4f3
mykothai <mike.thai2303@gmail.com>
2020-04-01 16:59:33 -0700

    Create adjacency list

--- A7/java code/src/Main.java
+++ A7/java code/src/Main.java
@@ -1,3 +1,4 @@
+import Graph.AdjacencyList;
 import Graph.AdjacencyMatrix;
 
 public class Main {
@@ -7,12 +8,13 @@ public class Main {
 
         PrimsMinimumSpanningTree primMST = new PrimsMinimumSpanningTree();
         AdjacencyMatrix adjMatrix = new AdjacencyMatrix(numberVertices);
-        //AdjacencyList adjList = new AdjacencyList(numberVertices);
+        AdjacencyList adjList = new AdjacencyList(numberVertices);
 
         // run time analysis
         long startTime = 0;
         long endTime = 0;
 
+        // Create the adjacency matrix
         adjMatrix.addEdge(0, 1, 4);
         adjMatrix.addEdge(0, 7, 8);
         adjMatrix.addEdge(1, 7, 11);
@@ -30,12 +32,39 @@ public class Main {
 
         adjMatrix.printGraph();
 
-        System.out.println("----- Prim's MST with Adjacency Matrix -----\n");
+        System.out.println("\n ----- Prim's MST with Adjacency Matrix -----\n");
         startTime = System.nanoTime();
 
         primMST.primsAdjacencyMatrix(adjMatrix);
 
         endTime = System.nanoTime();
         System.out.println("Execution time: " + String.format("%.6f", (float)(endTime - startTime) / 1000000) + " ms.\n");
+
+        // Create the adjacency list
+        adjList.addEdge(0, 1, 4);
+        adjList.addEdge(0, 7, 8);
+        adjList.addEdge(1, 7, 11);
+        adjList.addEdge(1, 2, 8);
+        adjList.addEdge(2, 3, 7);
+        adjList.addEdge(2, 5, 4);
+        adjList.addEdge(2, 8, 2);
+        adjList.addEdge(3, 4, 9);
+        adjList.addEdge(3, 5, 14);
+        adjList.addEdge(4, 5, 10);
+        adjList.addEdge(5, 6, 2);
+        adjList.addEdge(6, 8, 6);
+        adjList.addEdge(6, 7, 1);
+        adjList.addEdge(7, 8, 7);
+
+        adjList.printGraph();
+
+        System.out.println("\n ----- Prim's MST with Adjacency List -----\n");
+        startTime = System.nanoTime();
+
+        primMST.primsAdjacencyList(adjList);
+
+        endTime = System.nanoTime();
+        System.out.println("Execution time: " + String.format("%.6f", (float)(endTime - startTime) / 1000000) + " ms.\n");
+
     }
 }
