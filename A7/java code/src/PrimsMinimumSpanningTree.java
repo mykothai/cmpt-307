9f5d4fc611bb0d917bc559d34cbec77c1470c1aa
mykothai <mike.thai2303@gmail.com>
2020-04-01 16:06:12 -0700

    Create classes

---
+++ A7/java code/src/PrimsMinimumSpanningTree.java
@@ -0,0 +1,18 @@
+public class PrimsMinimumSpanningTree {
+
+    public PrimsMinimumSpanningTree() {}
+
+    public void primsAdjacencyList(AdjacencyMatrix graph) {
+        if (graph == null) {
+            throw new NullPointerException("Adjacency list is null.");
+        }
+
+    }
+
+    public void primsAdjacencyMatrix(AdjacencyMatrix graph) {
+        if (graph == null) {
+            throw new NullPointerException("Adjacency matrix is null.");
+        }
+
+    }
+}
