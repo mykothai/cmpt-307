c228434240c8db71c2c355db44cd45ced6a77021
mykothai <mike.thai2303@gmail.com>
2020-04-03 20:32:23 -0700

    Implement main for figure 1 graph

--- A7/java code/src/Main.java
+++ A7/java code/src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         int numberVertices = 9;
 
-        PrimsMinimumSpanningTree primMST = new PrimsMinimumSpanningTree();
+        PrimsMSTAdjacencyMatrix primsMSTAdjacencyMatrix = new PrimsMSTAdjacencyMatrix();
+        PrimsMSTAdjacencyList primsMSTAdjacencyList = new PrimsMSTAdjacencyList();
         AdjacencyMatrix adjMatrix = new AdjacencyMatrix(numberVertices);
         AdjacencyList adjList = new AdjacencyList(numberVertices);
 
@@ -29,16 +30,27 @@ public class Main {
         adjMatrix.addEdge(6, 8, 6);
         adjMatrix.addEdge(6, 7, 1);
         adjMatrix.addEdge(7, 8, 7);
-
-        adjMatrix.printGraph();
+//        adjMatrix.printGraph();
 
         System.out.println("----- Prim's MST with Adjacency Matrix -----\n");
         startTime = System.nanoTime();
 
-        primMST.primsAdjacencyMatrix(adjMatrix);
+        primsMSTAdjacencyMatrix.primsMST(adjMatrix, 0);
 
         endTime = System.nanoTime();
         System.out.println("Execution time: " + String.format("%.6f", (float)(endTime - startTime) / 1000000) + " ms.\n");
+
+//        System.out.println("ADJACENCY MATRIX SOLUTION");
+//        AdjacencyMatrix solution1 = new AdjacencyMatrix(numberVertices);
+//        solution1.addEdge(0, 1, 4);
+//        solution1.addEdge(0, 7, 8);
+//        solution1.addEdge(2, 3, 7);
+//        solution1.addEdge(2, 5, 4);
+//        solution1.addEdge(2, 8, 2);
+//        solution1.addEdge(3, 4, 9);
+//        solution1.addEdge(5, 6, 2);
+//        solution1.addEdge(6, 7, 1);
+//        solution1.printGraph();
 
         // Create the adjacency list
         adjList.addEdge(0, 1, 4);
@@ -55,16 +67,26 @@ public class Main {
         adjList.addEdge(6, 8, 6);
         adjList.addEdge(6, 7, 1);
         adjList.addEdge(7, 8, 7);
-
-        adjList.printGraph();
+//        adjList.printGraph();
 
         System.out.println("----- Prim's MST with Adjacency List -----\n");
         startTime = System.nanoTime();
 
-        primMST.primsAdjacencyList(adjList);
+        primsMSTAdjacencyList.primsMST(adjList, 0);
 
         endTime = System.nanoTime();
         System.out.println("Execution time: " + String.format("%.6f", (float)(endTime - startTime) / 1000000) + " ms.\n");
 
+//        System.out.println("ADJACENCY LIST SOLUTION");
+//        AdjacencyList solution = new AdjacencyList(numberVertices);
+//        solution.addEdge(0, 1, 4);
+//        solution.addEdge(0, 7, 8);
+//        solution.addEdge(2, 3, 7);
+//        solution.addEdge(2, 5, 4);
+//        solution.addEdge(2, 8, 2);
+//        solution.addEdge(3, 4, 9);
+//        solution.addEdge(5, 6, 2);
+//        solution.addEdge(6, 7, 1);
+//        solution.printGraph();
     }
 }
